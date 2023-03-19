994ce2542b84a8e2d052407d1dbb1a5e08555caf
OksanaZhuravel <opuseva@mail.ru>
2023-03-19 15:55:47 +0400

    fix: change page About

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import { Component } from 'react';
 
-export default class About extends React.Component {
-  render(): React.ReactNode {
+export default class About extends Component {
+  render() {
     return (
-      <div>
-        About
-        {/* <button onClick={() => this.navigate('/')}>Go Back Home Page</button> */}
+      <div className="about">
+        <p className="about__title">
+          Shop is an online marketplace offering a wide range of products from various categories
+          including electronics, clothing, home goods, and more. We strive to provide high-quality
+          products at competitive prices, and offer fast and reliable shipping options to ensure a
+          smooth and hassle-free shopping experience for our customers. Shop with us today and
+          discover our latest deals and promotions!
+        </p>
       </div>
     );
   }
