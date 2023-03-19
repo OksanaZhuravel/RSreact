5d8ef5faa642d4774607962b1f8bad9e1e7e3e23
OksanaZhuravel <opuseva@mail.ru>
2023-03-19 16:04:45 +0400

    fix: change style pages

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -3,7 +3,7 @@ import { Component } from 'react';
 export default class NotFound extends Component {
   render(): React.ReactNode {
     return (
-      <div>
+      <div style={{ textAlign: 'center', width: '100%' }}>
         <h2>Oops!</h2>
         <p>Sorry, an unexpected error has occurred.</p>
       </div>
