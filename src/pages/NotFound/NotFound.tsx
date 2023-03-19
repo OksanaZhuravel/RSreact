8e2b197a1f20b508307fb2c074b8303e29157e48
OksanaZhuravel <opuseva@mail.ru>
2023-03-19 15:13:17 +0400

    fix: change page NotFound

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,13 +1,11 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import { Component } from 'react';
 
-export default class NotFound extends React.Component {
+export default class NotFound extends Component {
   render(): React.ReactNode {
     return (
       <div>
         <h2>Oops!</h2>
         <p>Sorry, an unexpected error has occurred.</p>
-        <Link to={`/`}>Link Home page</Link>
       </div>
     );
   }
