0237b4070c1d1e5eef372d1b5a75808957c84058
OksanaZhuravel <opuseva@mail.ru>
2023-03-19 15:02:50 +0400

    fix: chahge project architecture

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Layout from './pages/Layout/Layout';
+import NotFound from './pages/NotFound/NotFound';
+import About from './pages/About/About';
+
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/" element={<Layout />} />
+            <Route path="/about" element={<About />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </main>
+      </>
+    );
+  }
+}
+
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
 import './index.css';
-import { RouterProvider } from 'react-router-dom';
-import router from './router/router';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>
 );
