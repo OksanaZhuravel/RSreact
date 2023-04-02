0b97201cb00c0c25bb8974e800a04d3c729d69b5
OksanaZhuravel <opuseva@mail.ru>
2023-04-02 13:43:11 +0400

    fix: change Layout

--- src/pages/Layout/Layout.tsx
+++ src/pages/Layout/Layout.tsx
@@ -1,33 +1,30 @@
-import { Component } from 'react';
 import SearchBar from '../../components/SearchBar/SearchBar';
 import Card from '../../components/Card/Card';
 import data from '../../assets/data.json';
 
-class Layout extends Component {
-  render() {
-    return (
-      <div className="main">
-        <SearchBar />
-        <div className="main__cards" data-testid="cards-list">
-          {data.map((item) => {
-            return (
-              <Card
-                title={item.title}
-                key={item.id}
-                thumbnail={item.thumbnail}
-                rating={item.rating}
-                price={item.price}
-                description={item.description}
-                discountPercentage={item.discountPercentage}
-                category={item.category}
-                brand={item.brand}
-              />
-            );
-          })}
-        </div>
+function Layout() {
+  return (
+    <div className="main">
+      <SearchBar />
+      <div className="main__cards" data-testid="cards-list">
+        {data.map((item) => {
+          return (
+            <Card
+              title={item.title}
+              key={item.id}
+              thumbnail={item.thumbnail}
+              rating={item.rating}
+              price={item.price}
+              description={item.description}
+              discountPercentage={item.discountPercentage}
+              category={item.category}
+              brand={item.brand}
+            />
+          );
+        })}
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Layout;
