f6b5f641f2aff7ebcf933788f536ef2810a04950
OksanaZhuravel <opuseva@mail.ru>
2023-04-09 16:45:30 +0400

    fix: change Card

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,60 +1,35 @@
-import React from 'react';
+import { DataApi } from '../../types/types';
 import './card.scss';
 
 interface CardProps {
-  title: string;
-  description: string;
-  thumbnail: string;
-  price: number;
-  discountPercentage: number;
-  category: string;
-  brand: string;
-  rating: number;
+  item: DataApi;
 }
 
-const Card: React.FC<CardProps> = ({
-  title,
-  description,
-  thumbnail,
-  price,
-  discountPercentage,
-  category,
-  brand,
-  rating,
-}) => {
+const Card: React.FC<CardProps> = ({ item }) => {
+  const { name, status, species, gender, origin, location, image } = item;
+
   return (
-    <>
-      <div className="card" data-testid="card">
-        <img className="card__image" src={thumbnail} alt={title} />
-        <div className="card__info">
-          <h3 className="card__title">{title}</h3>
-          <p>
-            <span className="card__text">Category:</span>
-            <span className="card__category"> {category}</span>
-          </p>
-          <p>
-            <span className="card__text">Brand:</span>
-            <span className="card__brand"> {brand}</span>
-          </p>
-          <p>
-            <span className="card__text">Description:</span>
-            <span className="card__description"> {description}</span>
-          </p>
-          <p>
-            <span className="card__text">Raiting:</span>
-            <span className="card__rating"> {rating}</span>
-          </p>
-          <p>
-            <span className="card__text">price:</span>
-            <span className="card__price"> {price} $</span>
-          </p>
-          <p>
-            <span className="card__text">discount:</span>
-            <span className="card__discount"> {discountPercentage} %</span>
-          </p>
-        </div>
+    <div className="card">
+      <img src={image} alt={name} />
+      <h3>{name}</h3>
+      <div className="card__inner">
+        <p>
+          Status: <span className="card__color">{status}</span>
+        </p>
+        <p>
+          Species: <span className="card__color">{species}</span>
+        </p>
+        <p>
+          Gender: <span className="card__color">{gender}</span>
+        </p>
+        <p>
+          Origin: <span className="card__color">{origin.name}</span>
+        </p>
+        <p>
+          Location: <span className="card__color">{location.name}</span>
+        </p>
       </div>
-    </>
+    </div>
   );
 };
 
