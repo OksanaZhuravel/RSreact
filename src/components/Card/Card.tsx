5abfcf6aba979bfa0464b0be6a96da31567a4cc3
OksanaZhuravel <opuseva@mail.ru>
2023-04-09 17:37:09 +0400

    fix: change components

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,34 +1,40 @@
 import { DataApi } from '../../types/types';
 import './card.scss';
-
+import Modal from '../../components/Modal/Modal';
+import { useState } from 'react';
 interface CardProps {
   item: DataApi;
 }
 
 const Card: React.FC<CardProps> = ({ item }) => {
   const { name, status, species, gender, origin, location, image } = item;
-
+  const [modal, setModal] = useState(false);
   return (
     <div className="card">
       <img src={image} alt={name} />
       <h3>{name}</h3>
-      <div className="card__inner">
-        <p>
-          Status: <span className="card__color">{status}</span>
-        </p>
-        <p>
-          Species: <span className="card__color">{species}</span>
-        </p>
-        <p>
-          Gender: <span className="card__color">{gender}</span>
-        </p>
-        <p>
-          Origin: <span className="card__color">{origin.name}</span>
-        </p>
-        <p>
-          Location: <span className="card__color">{location.name}</span>
-        </p>
-      </div>
+      <button onClick={() => setModal(true)}>Information</button>
+      <Modal visible={modal} setVisible={setModal}>
+        <img src={image} alt={name} />
+        <h3>{name}</h3>
+        <div className="card__inner">
+          <p>
+            Status: <span className="card__color">{status}</span>
+          </p>
+          <p>
+            Species: <span className="card__color">{species}</span>
+          </p>
+          <p>
+            Gender: <span className="card__color">{gender}</span>
+          </p>
+          <p>
+            Origin: <span className="card__color">{origin.name}</span>
+          </p>
+          <p>
+            Location: <span className="card__color">{location.name}</span>
+          </p>
+        </div>
+      </Modal>
     </div>
   );
 };
--- src/pages/Layout/Layout.tsx
+++ src/pages/Layout/Layout.tsx
@@ -1,33 +1,3 @@
-// import SearchBar from '../../components/SearchBar/SearchBar';
-// import Card from '../../components/Card/Card';
-// import data from '../../assets/data.json';
-
-// function Layout() {
-//   return (
-//     <div className="main">
-//       <SearchBar />
-//       <div className="main__cards" data-testid="cards-list">
-//         {data.map((item) => {
-//           return (
-//             <Card
-//               title={item.title}
-//               key={item.id}
-//               thumbnail={item.thumbnail}
-//               rating={item.rating}
-//               price={item.price}
-//               description={item.description}
-//               discountPercentage={item.discountPercentage}
-//               category={item.category}
-//               brand={item.brand}
-//             />
-//           );
-//         })}
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Layout;
 import React, { useState, useEffect } from 'react';
 import SearchBar from '../../components/SearchBar/SearchBar';
 import Card from '../../components/Card/Card';
